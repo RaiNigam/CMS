696055ddf14b5a7993508660ada273c0cecc63fe
Zeroflash1 <nigamrai2020@gmail.com>
2023-06-13 16:30:01 +0545

    addUser.php and view.php added

--- Admin/addItems.php
+++ Admin/addItems.php
@@ -1,3 +1,56 @@
 <?php
 include '../Common/header.php';
 ?>
+<head>
+    <link rel="stylesheet" href="../css/addItems.css">
+</head>
+<body>
+    <section>
+        <h1 style="text-align:center;margin:20px 0 20px 0">Add Items</h1>
+    <table border="1" cellspacing="0" >
+        <thead>
+            <tr>
+                <th style="witdh:40px">S.No.</th>
+                <th>Item Name</th>
+                <th>Price</th>
+                <th>Remarks</th>    
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>Samosa</td>
+                <td>Rs. 25</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>Samosa</td>
+                <td>Rs. 25</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>Samosa</td>
+                <td>Rs. 25</td>
+                <td></td>
+            </tr>
+
+            <tr>
+                <td>1</td>
+                <td>Samosa</td>
+                <td>Rs. 25</td>
+                <td>Sold Out</td>
+            </tr>
+
+            <tr>
+                <td>1</td>
+                <td>Samosa</td>
+                <td>Rs. 25</td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+    </section>
+  
+</body>
